9bdc37f53d3aea6ac16006caa1cf6639e8e5e640
Farhan Ali <2206farhan@gmail.com>
2023-08-05 11:29:47 +0530

    Update client id

--- launch.js
+++ launch.js
@@ -2,10 +2,9 @@ function getFhirData() {
     const clientId = document.querySelector('input').value;
     console.log(clientId);
     FHIR.oauth2.authorize({
-      'client_id': clientId,
+      'client_id': '6267b480-d0e8-445b-bc76-3691adc4ef04',
       'scope': 'user/Patient.read user/Practitioner.read launch openid profile offline_access fhirUser',
-      // 'redirect_uri': 'https://roshithzoft.github.io/bay-smart-on-fhir-sample-app/app.html'
-      'redirect_uri': 'DS4U PUBLIC SERVER URL/app.html'  //Take help from mohit regarding this.
+      'redirect_uri': 'https://raw.githack.com/farhan2206/bay-smart-on-fhir-sample-app/main/app.html'
     });
   }
   
