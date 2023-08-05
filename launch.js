66129c9ba02ecef45b797c9042e9da48ea967eb7
Farhan Ali <2206farhan@gmail.com>
2023-08-05 12:13:22 +0530

    Add server url

--- launch.js
+++ launch.js
@@ -2,6 +2,7 @@ function getFhirData() {
     const clientId = document.querySelector('input').value;
     console.log(clientId);
     FHIR.oauth2.authorize({
+      'serverUrl'  : "http://r3.smarthealthit.org",
       'client_id': clientId,
       'scope': 'user/Patient.read user/Practitioner.read launch openid profile offline_access fhirUser',
       'redirect_uri': 'https://raw.githack.com/farhan2206/bay-smart-on-fhir-sample-app/main/app.html'
