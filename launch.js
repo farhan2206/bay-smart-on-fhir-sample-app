11a6b70ab0e484e40b05f50cdadbe803e3d252a5
Farhan Ali <2206farhan@gmail.com>
2023-08-05 11:31:03 +0530

    remove clientId harcoded

--- launch.js
+++ launch.js
@@ -2,7 +2,7 @@ function getFhirData() {
     const clientId = document.querySelector('input').value;
     console.log(clientId);
     FHIR.oauth2.authorize({
-      'client_id': '6267b480-d0e8-445b-bc76-3691adc4ef04',
+      'client_id': clientId,
       'scope': 'user/Patient.read user/Practitioner.read launch openid profile offline_access fhirUser',
       'redirect_uri': 'https://raw.githack.com/farhan2206/bay-smart-on-fhir-sample-app/main/app.html'
     });
